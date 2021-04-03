699390f069fb45e2f6d82006bc3ef0d4b5a7b9b8
jibranbinsaleem <40163456+jibranbinsaleem@users.noreply.github.com>
2021-04-03 20:34:11 +0500

    included lots of stuf

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'c6742aaffd21abae5b35f5a4'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
-from market import routes
+from market import routes 
